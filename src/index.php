9c91662ce7d318f0eeee715d76995ff19a8190af
Ollie Armstrong <ollie@armstrong.io>
2017-08-02 17:37:55 +0100

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php $hostname = `hostname`; ?>
+
+<html>
+<body>
+
+<p><?php echo "Hello World! This is PHP running from the system: $hostname"; ?></p>
+
+<img src="kitten.jpg">
+
+</body>
+</html>
